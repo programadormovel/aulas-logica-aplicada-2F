4df354f9d3f37068f4a8492566e5bebc8f8a1d56
programadormovel <programadormovel@gmail.com>
2023-05-18 11:06:20 -0300

    aula classe Pessoa inicial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        
+        // Aula de Orientação à Objetos
+        // Declarar objetos
+        Pessoa adao;
+        // Instanciar objetos
+        adao = new Pessoa();
+        Pessoa eva = new Pessoa();
+        // Definição do formato
+        adao.nome = "Adão";
+        adao.sobrenome = "Silva";
+        eva.nome = "Eva";
+        eva.sobrenome = "Silva";
+        // Acionar comportamento
+        adao.falar();
+        eva.falar();
+
+
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,10 @@
+public class Pessoa {
+    // Atributos da classe
+    String nome;
+    String sobrenome;
+    // Método da classe
+    public void falar(){
+        System.out.println("falei");
+    }
+
+}
